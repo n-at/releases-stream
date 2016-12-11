19ee72dcf05fd8f5c3df1b1e60da87fd284ef4fe
Alexey Nurgaliev <atnurgaliev@gmail.com>
2016-12-11 17:31:54 +0300

    mail template margins

--- src/main/java/ru/doublebyte/releasesstream/Runner.java
+++ src/main/java/ru/doublebyte/releasesstream/Runner.java
@@ -4,6 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import ru.doublebyte.releasesstream.db.Release;
+import ru.doublebyte.releasesstream.feed.AtomReader;
+import ru.doublebyte.releasesstream.feed.FeedEntry;
+import ru.doublebyte.releasesstream.github.Repository;
 import ru.doublebyte.releasesstream.mail.MailMessageSender;
 
 import java.time.LocalDateTime;
@@ -22,18 +25,18 @@ public class Runner implements CommandLineRunner {
 
     @Override
     public void run(String... strings) throws Exception {
+        AtomReader reader = new AtomReader();
+        List<FeedEntry> feedEntries = reader.read("https://github.com/docker/docker/releases.atom");
         List<Release> releases = new ArrayList<>();
 
-        Release release1 = new Release();
-        release1.setRepositoryName("n-at/arbirter");
-        release1.setRepositoryUrl("https://github.com/n-at/arbirter");
-        release1.setRepositoryDescription("Description text text text text text");
-        release1.setTitle("v.1.0.0-RELEASE");
-        release1.setDate(LocalDateTime.now());
-        release1.setAuthor("John Doe");
-        release1.setContent("djad kjhsjfgsjfg sf jhsf jsgfj sgfjgsj fsgf gsj");
+        Repository repository = new Repository();
+        repository.setName("docker/docker");
+        repository.setUrl("https://github.com/docker/docker");
+        repository.setDescription("Docker - the open-source application container engine");
 
-        releases.add(release1);
+        feedEntries.forEach(it -> {
+            releases.add(new Release(repository, it));
+        });
 
         mailMessageSender.sendReleases(releases);
     }
